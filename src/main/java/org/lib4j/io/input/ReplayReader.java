615c5eca2fb037d9cac622e365222d18c84546d0
Seva Safris <seva@safris.org>
2018-07-24 15:24:36 +0700

    Rewrite ReplayInputStream

--- src/main/java/org/lib4j/io/input/ReplayReader.java
+++ src/main/java/org/lib4j/io/input/ReplayReader.java
@@ -16,22 +16,43 @@
 
 package org.lib4j.io.input;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FilterInputStream;
+import java.io.CharArrayWriter;
+import java.io.FilterReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.Reader;
 
-public class ReplayInputStream extends FilterInputStream {
-  private class ReplayByteArrayOutputStream extends ByteArrayOutputStream {
+public class ReplayReader extends FilterReader {
+  protected class ReadbackCharArrayWriter extends CharArrayWriter {
     private int length;
     private int mark;
 
-    public ReplayByteArrayOutputStream(final int size) {
+    public ReadbackCharArrayWriter(final int size) {
       super(size);
     }
 
-    public ReplayByteArrayOutputStream() {
+    public ReadbackCharArrayWriter() {
       super();
+    }
+
+    @Override
+    public CharArrayWriter append(final char c) {
+      final CharArrayWriter writer = super.append(c);
+      ++length;
+      return writer;
+    }
+
+    @Override
+    public CharArrayWriter append(final CharSequence csq) {
+      final CharArrayWriter writer = super.append(csq);
+      length += csq.length();
+      return writer;
+    }
+
+    @Override
+    public CharArrayWriter append(final CharSequence csq, final int start, final int end) {
+      final CharArrayWriter writer = super.append(csq, start, end);
+      length += end - start;
+      return writer;
     }
 
     @Override
@@ -41,25 +62,43 @@ public class ReplayInputStream extends FilterInputStream {
     }
 
     @Override
-    public void write(final byte[] c, final int off, final int len) {
+    public void write(final char[] c, final int off, final int len) {
       super.write(c, off, len);
       length += len;
     }
 
+    @Override
+    public void write(final String str, final int off, final int len) {
+      super.write(str, off, len);
+      length += len;
+    }
+
+    @Override
+    public void write(final char[] cbuf) throws IOException {
+      super.write(cbuf);
+      length += cbuf.length;
+    }
+
+    @Override
+    public void write(final String str) throws IOException {
+      super.write(str);
+      length += str.length();
+    }
+
     public int read() {
       if (!isReadable())
-        throw new IllegalStateException("Stream has not been closed");
+        throw new IllegalStateException("Reader has not been closed");
 
       return count == length ? -1 : buf[count++];
     }
 
-    public int read(final byte[] b) {
+    public int read(final char[] b) {
       return read(b, 0, b.length);
     }
 
-    public int read(final byte[] b, final int off, final int len) {
+    public int read(final char[] b, final int off, final int len) {
       if (!isReadable())
-        throw new IllegalStateException("Stream has not been closed");
+        throw new IllegalStateException("Reader has not been closed");
 
       final int check = length - count - len;
       final int length = check < 0 ? len + check : len;
@@ -70,7 +109,7 @@ public class ReplayInputStream extends FilterInputStream {
 
     public long skip(final long n) {
       if (!isReadable())
-        throw new IllegalStateException("Stream has not been closed");
+        throw new IllegalStateException("Reader has not been closed");
 
       if (n <= 0)
         return 0;
@@ -96,21 +135,21 @@ public class ReplayInputStream extends FilterInputStream {
     }
 
     @Override
-    public void close() throws IOException {
+    public void close() {
       count = 0;
     }
   }
 
-  protected final ReplayByteArrayOutputStream buffer;
+  protected final ReadbackCharArrayWriter buffer;
 
-  public ReplayInputStream(final InputStream in, final int size) {
+  public ReplayReader(final Reader in, final int size) {
     super(in);
-    this.buffer = new ReplayByteArrayOutputStream(size);
+    this.buffer = new ReadbackCharArrayWriter(size);
   }
 
-  public ReplayInputStream(final InputStream in) {
+  public ReplayReader(final Reader in) {
     super(in);
-    this.buffer = new ReplayByteArrayOutputStream();
+    this.buffer = new ReadbackCharArrayWriter();
   }
 
   @Override
@@ -126,7 +165,7 @@ public class ReplayInputStream extends FilterInputStream {
   }
 
   @Override
-  public int read(final byte[] b) throws IOException {
+  public int read(final char[] b) throws IOException {
     if (buffer.isReadable())
       return buffer.read(b);
 
@@ -138,7 +177,7 @@ public class ReplayInputStream extends FilterInputStream {
   }
 
   @Override
-  public int read(final byte[] b, final int off, final int len) throws IOException {
+  public int read(final char[] b, final int off, final int len) throws IOException {
     if (buffer.isReadable())
       return buffer.read(b, off, len);
 
@@ -147,11 +186,6 @@ public class ReplayInputStream extends FilterInputStream {
       buffer.write(b, off, by);
 
     return by;
-  }
-
-  @Override
-  public int available() throws IOException {
-    return super.available() + buffer.length - buffer.size();
   }
 
   @Override
@@ -167,14 +201,7 @@ public class ReplayInputStream extends FilterInputStream {
   }
 
   @Override
-  public void close() throws IOException {
-    in.close();
-    buffer.close();
-  }
-
-  @Override
   public void mark(final int readlimit) {
-    in.mark(readlimit);
     buffer.mark();
   }
 
@@ -186,5 +213,11 @@ public class ReplayInputStream extends FilterInputStream {
   @Override
   public void reset() throws IOException {
     buffer.reset();
+  }
+
+  @Override
+  public void close() throws IOException {
+    in.close();
+    buffer.close();
   }
 }
