ce71d8a713687f5b5676b95ca2cc78b724fb151b
Seva Safris <seva@safris.org>
2018-09-12 23:54:21 +0700

    Move io to fastjax

--- src/test/java/org/lib4j/io/ReplayInputStreamTest.java
+++ src/test/java/org/lib4j/io/ReplayInputStreamTest.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016 lib4j
+/* Copyright (c) 2016 FastJAX
  *
  * Permission is hereby granted, free of bytege, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
